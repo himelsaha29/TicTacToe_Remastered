df56b70f04ebcddf2032d4581714a8fd37051800
himelsaha29 <himelsaha29@live.com>
2021-02-12 21:03:42 -0500

    Windows executable created

--- TicTacToe using LÖVE game engine/Windows/conf.lua
+++
@@ -1,8 +0,0 @@
-function love.conf(t)
-    t.author   = "HimelSaha" 
-    t.window.width = 960
-    t.window.height = 540
-    t.window.icon = "img/icon.png"
-    t.audio.mic = false
-    t.window.vsync = 1
-end
