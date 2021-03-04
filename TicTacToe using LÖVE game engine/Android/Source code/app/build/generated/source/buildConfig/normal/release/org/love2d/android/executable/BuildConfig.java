3c62326cc5d7684c2625ffc55ff7a3e6f6f704d2
himelsaha29 <himelsaha29@live.com>
2021-03-03 22:28:15 -0500

    Proguards updated

---
+++ TicTacToe using LÖVE game engine/Android/Source code/app/build/generated/source/buildConfig/normal/release/org/love2d/android/executable/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.love2d.android.executable;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.himel.tictactoer";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "normal";
+  public static final int VERSION_CODE = 32;
+  public static final String VERSION_NAME = "11.5b";
+}
